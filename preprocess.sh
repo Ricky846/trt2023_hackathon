40ef40690c3ee83204b02cfa3f8891b12ad83dcc
Ricky846 <437986840@qq.com>
2023-07-27 11:23:30 +0000

    Changes to be committed: 	modified:   preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# 配置文件的路径
+config_file="/home/player/ControlNet/models/cldm_v15.yaml"
+
+# 使用 sed 命令进行替换
+sed -i 's/use_checkpoint: true/use_checkpoint: False/g' "$config_file"
+
 python3 torch2engine.py
