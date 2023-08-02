45eca436cd3f49b5d6ba8a6d5ae53772d8cd100d
Ricky846 <437986840@qq.com>
2023-08-02 08:30:09 +0000

    Changes to be committed: 	modified:   controlnet2engine.py 	modified:   unet2engine.py

--- unet2engine.py
+++ unet2engine.py
@@ -4,6 +4,7 @@ import onnx
 import tensorrt as trt 
 import os
 
+import onnx_graphsurgeon as gs
 from canny2image_torch import hackathon
 
 def create_engine(onnx_model):
@@ -64,8 +65,8 @@ hk.initialize()
 model = hk.model
 unet = model.model.diffusion_model
 
-unet_onnx_path = "unet.onnx"
-unet_engine_path = "unet.engine"
+unet_onnx_path = "unet_temp.onnx"
+# unet_engine_path = "unet.engine"
 
 diffusion_model_input1 = torch.zeros((1,4,32,48), dtype=torch.float32, device='cuda')
 diffusion_model_input2 = torch.zeros(1, dtype=torch.int32, device='cuda')
@@ -115,17 +116,18 @@ torch.onnx.export(unet,
                     diffusion_model_input4_10, diffusion_model_input4_11, diffusion_model_input4_12, diffusion_model_input4_13]),
                     unet_onnx_path,
                     export_params=True,
-                    opset_version=17,
+                    opset_version=16,
                     do_constant_folding=True,
                     # keep_initializers_as_inputs=True,
                     input_names = input_names, 
                     dynamic_axes = dynamic_table)
 
-# if not os.path.isfile(clip_engine_path):
-# unet_onnx = onnx.load(unet_onnx_path)
-# unet_engine = create_engine(clip_onnx)
-# with open(unet_engine_path, mode='wb') as f: 
-#     f.write(unet_engine)
-#     print("generating file done!") 
+net_onnx = onnx.load(unet_onnx_path)
+net_graph = gs.import_onnx(net_onnx)
+net_graph = net_graph.fold_constants()
+net_graph = net_graph.cleanup()
+net_onnx = gs.export_onnx(net_graph)
+
+onnx.save(net_onnx, "unet.onnx", save_as_external_data=True)
 
 os.system("trtexec --onnx=unet.onnx --saveEngine=unet.engine --fp16 --builderOptimizationLevel=5 --inputIOFormats=fp32:chw,int32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw,fp32:chw --optShapes=sample:1x4x32x48,timestep:1,encoder_hidden_states:1x77x768,control_input_1:1x320x32x48,control_input_2:1x320x32x48,control_input_3:1x320x32x48,control_input_4:1x320x16x24,control_input_5:1x640x16x24,control_input_6:1x640x16x24,control_input_7:1x640x8x12,control_input_8:1x1280x8x12,control_input_9:1x1280x8x12,control_input_10:1x1280x4x6,control_input_11:1x1280x4x6,control_input_12:1x1280x4x6,control_input_13:1x1280x4x6")
